8f647575909b637f5137597fa469e665654029fb
ThanhHoa_JIKAY <hoanguyenthanh07@gmail.com>
2025-07-18 03:04:44 +0700

    up
    up

--- frontend/web/next.config.mjs
+++ frontend/web/next.config.mjs
@@ -1,26 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  images: {
-    unoptimized: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placeholder.svg',
+      },
+    ],
+    unoptimized: true,
   },
 };
 
