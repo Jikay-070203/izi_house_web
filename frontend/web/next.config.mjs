7720cded9461b50caa8727e4d444245908a254a7
ThanhHoa_JIKAY <hoanguyenthanh07@gmail.com>
2025-07-19 01:58:10 +0700

    ss
    ss

--- frontend/web/next.config.mjs
+++ frontend/web/next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {},
+  // Add these configurations
+  webpack: (config, { isServer, dev }) => {
+    // Disable webpack cache in production
+    if (!dev && !isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
+  // Enable static export
+  output: 'export',
+  // Optional: Add basePath if your app is not served from the root
+  // basePath: '/your-base-path',
 };
 
 export default nextConfig;
