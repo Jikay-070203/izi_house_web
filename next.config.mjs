49439de858916572c6f3ff9dbbd7366fc73f83eb
ThanhHoa_JIKAY <hoanguyenthanh07@gmail.com>
2025-07-19 10:12:25 +0700

    s
    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 👈 Bắt buộc để dùng Cloudflare Pages static
-  images: {
-    unoptimized: true, // 👈 Bắt buộc nếu dùng <Image> của next/image
-  },
+  images: {},
 };
 
 export default nextConfig;
